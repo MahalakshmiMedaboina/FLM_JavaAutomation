69739d8b2038f9446541a3b3197449af0f44c3a7
MahalakshmiMedaboina <medaboinamahaa@gmail.com>
2026-05-16 23:43:56 +0530

    enhanced for loop: print fruits of string array

--- src/loops/ForLoopDemo.java
+++ src/loops/ForLoopDemo.java
@@ -17,16 +17,27 @@ public class ForLoopDemo {
 		
 		// WAP to print even nums from 10 to 1
 		
-		for(int i = 10; i>= 1; i-=2) {
-			System.out.println(i);
-		}
+//		for(int i = 10; i>= 1; i-=2) {
+//			System.out.println(i);
+//		}
 		
 		//------------------------------------------------------------
 		// WAP to print even nums from 1 to 10
 		
-		for(int i = 1; i<= 10; i++) {
-			System.out.println(i);
+//		for(int i = 1; i<= 10; i++) {
+//			System.out.println(i);
+//		}
+		
+		//-----------------------------------------------------------
+		//WAP to print array string elements using enhanced for loop
+		
+		String fruits[] = {"Apple", "Mango", "Banana"};
+		
+		for(String f : fruits) {
+			System.out.println("fruit : " + f);
 		}
+		
+		
 	}
 
 }
