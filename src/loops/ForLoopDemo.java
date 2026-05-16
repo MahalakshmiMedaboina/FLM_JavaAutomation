a3cc1b2c426f430357d14a96e360a6e7f3bd8499
MahalakshmiMedaboina <medaboinamahaa@gmail.com>
2026-05-16 23:12:51 +0530

    for loop: WAP to print 1 to 5

---
+++ src/loops/ForLoopDemo.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		// print numbers from 1 to 5
+		
+		for(int i=1; i<=5; i++) {
+			System.out.println("value of i is:"+i);
+		}
+
+	}
+
+}
