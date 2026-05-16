b76a4c6caeef816c9e2cfcd0acf125c8dd9bad16
MahalakshmiMedaboina <medaboinamahaa@gmail.com>
2026-05-16 23:31:38 +0530

    for loop: WAP to print even nums from 1 to 10

--- src/loops/ForLoopDemo.java
+++ src/loops/ForLoopDemo.java
@@ -20,7 +20,13 @@ public class ForLoopDemo {
 		for(int i = 10; i>= 1; i-=2) {
 			System.out.println(i);
 		}
-
+		
+		//------------------------------------------------------------
+		// WAP to print even nums from 1 to 10
+		
+		for(int i = 1; i<= 10; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
