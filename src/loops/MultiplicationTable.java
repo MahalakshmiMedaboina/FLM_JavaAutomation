c8fa9c19a7888636919d0ed8f2d60288ed9336d5
MahalakshmiMedaboina <medaboinamahaa@gmail.com>
2026-05-17 21:36:56 +0530

    WAP to print 11 to 15 tables using nested for loop

--- src/loops/MultiplicationTable.java
+++ src/loops/MultiplicationTable.java
@@ -3,19 +3,33 @@ package loops;
 public class MultiplicationTable {
 
 	public static void main(String[] args) {
-		// WAP to print 5 tables using nested for loop
+		// WAP to print 1 to 10 tables using nested for loop
 		
-		System.out.println("----Tables (1 to 5)----");
+//		System.out.println("----Tables (1 to 5)----");
+//		
+//		for(int i=1; i<= 5; i++) {
+//			
+//			System.out.println("Table of "+ i);
+//			
+//			for(int j=1; j <=10; j++) {
+//				System.out.println((i*j) + " ");
+//			}
+//		}
 		
-		for(int i=1; i<=5; i++) {
+		//----------------------------------------------------
+		// WAP to print 11 to 15 tables using nested for loop
+		
+		System.out.println("----Tables (11 to 15)----");
+		for(int i = 11; i<=15; i++) {
 			
-			System.out.println("Table of "+ i);
+			System.out.println("Table of "+ i); 
 			
-			for(int j=1; j <=10; j++) {
-				System.out.println((i*j) + " ");
+			for(int j=1; j<=10; j++) {
+				System.out.println(i + " x "+ j + " = " + (i*j));
 			}
+			
 		}
-
+		
 	}
 
 }
