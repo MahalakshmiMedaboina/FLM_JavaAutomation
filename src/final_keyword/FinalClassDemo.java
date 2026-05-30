b3a166fca569792356394f48cf579f60973bffe3
MahalakshmiMedaboina <medaboinamahaa@gmail.com>
2026-05-30 21:32:53 +0530

    final class demo

---
+++ src/final_keyword/FinalClassDemo.java
@@ -0,0 +1,15 @@
+package final_keyword;
+
+final class FinalClassDemo {
+		String name = "Final Class A";
+		{
+		System.out.println(name);
+		}
+}
+
+//class B extends A{
+//	// Attempting to extend the final class will cause a compile-time error 
+//}
+
+
+
